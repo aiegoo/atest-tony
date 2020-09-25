76be17dfd5fac95e26bf7b19bbdcda8e21fc051f
aiegoo <eozz21@gmail.com>
2020-09-25 16:34:16 +0900

    add xml json contents to views.py

--- restapi/views.py
+++ restapi/views.py
@@ -6,3 +6,13 @@ def home(request):
 def taskstring(request):
      result = 'Rest API stinrg!'
      return HttpResponse(result, content_type="text/plain")
+def taskxml(request):
+     result = '<employees><employee>  \
+                <firstName>John</firstName> <lastName>Doe</lastName> </employee> \
+               <employee>  \
+               <firstName>Anna</firstName> <lastName>Smith</lastName></employee> \
+           </employees>'
+
+     result = {"employees":[ { "firstName":"John", "lastName":"Doe" },  
+               { "firstName":"Anna", "lastName":"Smith" },
+               { "firstName":"Peter", "lastName":"Jones" } ]}
