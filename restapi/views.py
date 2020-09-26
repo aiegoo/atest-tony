5faabd0e1a5cfd5aae7b2541a6f77ec1b8340ff4
aiegoo <eozz21@gmail.com>
2020-09-26 10:32:46 +0900

    add template for weather and create models with openweahtermap

--- restapi/views.py
+++ restapi/views.py
@@ -2,22 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.urls import include, path
+from .form import CityForm
 
 # Create your views here.
 
 def home(request):
-     return HttpResponse("Hello, Django!")
-def taskstring(request):
-     result = 'Rest API stinrg!'
-     return HttpResponse(result, content_type="text/plain")
-def taskxml(request):
-     result = '<employees><employee>  \
-                <firstName>John</firstName> <lastName>Doe</lastName> </employee> \
-               <employee>  \
-               <firstName>Anna</firstName> <lastName>Smith</lastName></employee> \
-           </employees>'
+     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=22b142ec10552ddfa0aa658e9121f46e'
 
-     result = {"employees":[ { "firstName":"John", "lastName":"Doe" },  
-               { "firstName":"Anna", "lastName":"Smith" },
-               { "firstName":"Peter", "lastName":"Jones" } ]}
-     return JsonResponse(result)
+# def home(request):
+#      return HttpResponse("Hello, Django!")
+# def taskstring(request):
+#      result = 'Rest API stinrg!'
+#      return HttpResponse(result, content_type="text/plain")
+# def taskxml(request):
+#      result = '<employees><employee>  \
+#                 <firstName>John</firstName> <lastName>Doe</lastName> </employee> \
+#                <employee>  \
+#                <firstName>Anna</firstName> <lastName>Smith</lastName></employee> \
+#            </employees>'
+
+#      result = {"employees":[ { "firstName":"John", "lastName":"Doe" },  
+#                { "firstName":"Anna", "lastName":"Smith" },
+#                { "firstName":"Peter", "lastName":"Jones" } ]}
+#      return JsonResponse(result)
--- web_config/settings.py
+++ web_config/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'kn-kr'
+LANGUAGE_CODE = 'kn'
 
 TIME_ZONE = 'Asia/Seoul'
 
